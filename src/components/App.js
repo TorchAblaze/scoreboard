abeab926d2869d5a49f289de26436bb529fa0fc9
TorchAblaze <tiffanygreathead@gmail.com>
2021-08-10 01:02:49 -0700

    Add refactored code: counter state removed from Counter.js into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,44 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Player from "./Player";
+import AddPlayerForm from "./AddPlayerForm";
 
 class App extends Component {
   state = {
     players: [
       {
         name: "Guil",
+        score: 0,
         id: 1,
       },
       {
         name: "Treasure",
+        score: 0,
         id: 2,
       },
       {
         name: "Ashley",
+        score: 0,
         id: 3,
       },
       {
         name: "James",
+        score: 0,
         id: 4,
       },
     ],
+  };
+
+  handleScoreChange = (index, delta) => {
+    this.setState((prevState) => {
+      const updatedPlayers = [...prevState.players];
+      const updatedPlayer = { ...updatedPlayers[index] };
+      updatedPlayer.score += delta;
+      updatedPlayers[index] = updatedPlayer;
+      return {
+        players: updatedPlayers,
+      };
+    });
   };
 
   handleRemovePlayer = (id) => {
@@ -35,17 +52,21 @@ class App extends Component {
   render() {
     return (
       <div className="scoreboard">
-        <Header title="Scoreboard" totalPlayers={this.state.players.length} />
+        <Header title="Scoreboard" players={this.state.players} />
 
         {/* Players list */}
-        {this.state.players.map((player) => (
+        {this.state.players.map((player, index) => (
           <Player
             name={player.name}
+            score={player.score}
             id={player.id}
             key={player.id.toString()}
+            index={index}
+            changeScore={this.handleScoreChange}
             removePlayer={this.handleRemovePlayer}
           />
         ))}
+        <AddPlayerForm />
       </div>
     );
   }
